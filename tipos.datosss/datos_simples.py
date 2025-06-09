b4de64478ad98b56bc747fc739b4849634b83929
miguellineros <miguellineros7@gmail.com>
2025-06-09 19:52:56 -0400

    miguel

---
+++ tipos.datosss/datos_simples.py
@@ -0,0 +1,36 @@
+"cadena de texto"
+'otra cadena de texto'
+"""asdgfdg
+adfsg
+gafsd"""
+#esto es un string#
+print("esto es una cadena de texto")#permite en el terminal mostrar algo#
+print("esto es una cadena de texto con'comillas'")#o las comillas alreves#
+
+#datos tipo numericos
+# int
+numeros_enteros = 49
+#float
+numero_decimal = 56.32
+
+
+#datos de tipo logico
+# bool
+datos_verdaderos = True
+dato_falso = False
+
+cadena_de_texto_1 = "cadena de texto"
+cadena_de_texto_2 = 'otra cadena de texto'
+cadena_de_texto_3 = """asdgfdg
+adfsg
+gafsd"""
+
+print(cadena_de_texto_1)
+print(cadena_de_texto_2)
+print(cadena_de_texto_3)
+
+saludo = "buen dia "
+nombre = input("escriba su nombre:")
+texto_final = saludo + nombre
+print(texto_final)
+
